505a580ea564f6f4a2610b71eb46262295529bf5
starwing <weasley.wx@gmail.com>
2011-09-28 16:13:32 +0800

    add lua.h to md5.h

--- src/md5.h
+++ src/md5.h
@@ -8,9 +8,10 @@
 #ifndef md5_h
 #define md5_h
 
+#include <lua.h>
+
 
 #define HASHSIZE       16
-
 
 void md5 (const char *message, long len, char *output);
 LUALIB_API int luaopen_md5 (lua_State *L);
