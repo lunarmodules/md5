a43e3a6179fad23345225074360cb274fc76c24a
Tomas Guisasola <tomasguisasola@gmail.com>
2013-09-21 11:30:36 -0300

    ompatibility improvement, despite not being used.

--- src/compat-5.2.h
+++ src/compat-5.2.h
@@ -7,4 +7,9 @@
    (*(B)->p++ = (char)(c)))
 #endif
 
+#if LUA_VERSION_NUM==501
+/* Lua 5.1 */
+#define lua_rawlen lua_objlen
+#endif
+
 void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup);
