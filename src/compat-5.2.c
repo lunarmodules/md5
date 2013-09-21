d6719be4d52ca06e29bc5dd92c98fd03538ecec9
Tomas Guisasola <tomasguisasola@gmail.com>
2013-09-21 11:28:50 -0300

    Bug correction: the function didn't work with upvalues (thanks to Philipp Janda).

--- src/compat-5.2.c
+++ src/compat-5.2.c
@@ -7,14 +7,14 @@
 ** Adapted from Lua 5.2.0
 */
 void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
-  luaL_checkstack(L, nup, "too many upvalues");
+  luaL_checkstack(L, nup+1, "too many upvalues");
   for (; l->name != NULL; l++) {  /* fill the table with given functions */
     int i;
+    lua_pushstring(L, l->name);
     for (i = 0; i < nup; i++)  /* copy upvalues to the top */
       lua_pushvalue(L, -nup);
-    lua_pushstring(L, l->name);
     lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
-    lua_settable(L, -(nup + 3));
+    lua_settable(L, -3);
   }
   lua_pop(L, nup);  /* remove upvalues */
 }
